40191453cbecaaa47b4d51251cfb0ffcbebb3be7
Abhishek Panicker <abhishek.panicker@terrificminds.com>
2025-07-24 03:23:56 +0530

    fix: allow cors origin for server ip

--- app-backend/job_referral_backend/settings.py
+++ app-backend/job_referral_backend/settings.py
@@ -24,15 +24,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=u1r+r!h!rhya@jd)tqe0makr3dl$@=2yzl24^)p5r+wf3plz+'
+SECRET_KEY = "django-insecure-=u1r+r!h!rhya@jd)tqe0makr3dl$@=2yzl24^)p5r+wf3plz+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tm-referral.localhost','backend.tm-referral.localhost','localhost','127.0.0.1','129.159.224.253']
+ALLOWED_HOSTS = [
+    "tm-referral.localhost",
+    "backend.tm-referral.localhost",
+    "localhost",
+    "127.0.0.1",
+    "129.159.224.253",
+]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://tm-referral.localhost',
+    "http://tm-referral.localhost",
     "http://localhost:3000",
 ]
 
@@ -42,62 +48,63 @@ CORS_ALLOWED_ORIGINS = [
     "http://tm-referral.localhost",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
+    "http://129.159.224.253:8000",
 ]
 
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'apps.referral_app',
-    'rest_framework_simplejwt',
-    'drf_yasg',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "apps.referral_app",
+    "rest_framework_simplejwt",
+    "drf_yasg",
     "corsheaders",
-    'django_extensions',
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'job_referral_backend.urls'
+ROOT_URLCONF = "job_referral_backend.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'job_referral_backend.wsgi.application'
+WSGI_APPLICATION = "job_referral_backend.wsgi.application"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
 
-AUTH_USER_MODEL = 'referral_app.CustomUser'
+AUTH_USER_MODEL = "referral_app.CustomUser"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
@@ -118,16 +125,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -135,42 +142,37 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
 USE_TZ = True
 
 SWAGGER_SETTINGS = {
-    'DEFAULT_API_URL' : os.environ.get('SWAGGER_DEFAULT_API_URL', ""),
-    'USE_SESSION_AUTH': False,
-    'SECURITY_DEFINITIONS': {
-        'Bearer': {
-            'type': 'apiKey',
-            'name': 'Authorization',
-            'in': 'header'
-        }
+    "DEFAULT_API_URL": os.environ.get("SWAGGER_DEFAULT_API_URL", ""),
+    "USE_SESSION_AUTH": False,
+    "SECURITY_DEFINITIONS": {
+        "Bearer": {"type": "apiKey", "name": "Authorization", "in": "header"}
     },
-
 }
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 
-MEDIA_URL = '/uploads/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_URL = "/uploads/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=14),
@@ -180,10 +182,10 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": True,
 }
 
-EMAIL_BACKEND         = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST            = get_env_value('EMAIL_HOST','smtp.gmail.com' )
-EMAIL_HOST_USER       = get_env_value('EMAIL_HOST_USER','testmailadhd@gmail.com')
-EMAIL_HOST_PASSWORD   = get_env_value('EMAIL_HOST_PASSWORD','test')
-EMAIL_PORT            = get_env_value('EMAIL_PORT',587)
-EMAIL_USE_TLS         = True
-DEFAULT_FROM_EMAIL    = get_env_value('DEFAULT_FROM_EMAIL','testmailadhd@gmail.com')
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = get_env_value("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_HOST_USER = get_env_value("EMAIL_HOST_USER", "testmailadhd@gmail.com")
+EMAIL_HOST_PASSWORD = get_env_value("EMAIL_HOST_PASSWORD", "test")
+EMAIL_PORT = get_env_value("EMAIL_PORT", 587)
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = get_env_value("DEFAULT_FROM_EMAIL", "testmailadhd@gmail.com")
